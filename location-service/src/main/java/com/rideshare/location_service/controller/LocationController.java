13a8f1e8239f766f4ca000cd1aa5832e3fb94dc3
Vikas Dagur <vikasda@interrait.com>
2026-04-24 14:25:08 +0530

    location service

--- location-service/src/main/java/com/rideshare/location_service/controller/LocationController.java
+++ location-service/src/main/java/com/rideshare/location_service/controller/LocationController.java
@@ -1,15 +1,24 @@
 package com.rideshare.location_service.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rideshare.location_service.dto.DriverLocationRequest;
+import com.rideshare.location_service.dto.NearbyDriverResponse;
 import com.rideshare.location_service.service.LocationService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("api/v1/locations")
@@ -19,9 +28,24 @@ public class LocationController {
 
     private final LocationService locationService;
 
-    // driver 
-    public ResponseEntity<String> updateDriverLocation(@RequestBody DriverLocationRequest driverLocationRequest){
+    // driver
+    @PostMapping("/drivers/update")
+    public ResponseEntity<String> updateDriverLocation(@RequestBody DriverLocationRequest driverLocationRequest) {
         locationService.updateDriverLocation(driverLocationRequest);
         return ResponseEntity.ok("Driver location updated");
+    }
+
+    @GetMapping("/drivers/nearby")
+    public ResponseEntity<List<NearbyDriverResponse>> getNearByDrivers(
+            @RequestParam double longitude,
+            @RequestParam double latitude,
+            @RequestParam(defaultValue = "5.0") double radius) {
+        return ResponseEntity.ok(locationService.findNearbyDrivers(latitude, longitude, radius));
+    }
+
+    @DeleteMapping("/drivers/{driverID}")
+    public ResponseEntity<String> removeDriver(@PathVariable String driverID){
+        locationService.removeDriver(driverID);
+        return ResponseEntity.ok("driver removed successfully!")
     }
 }
--- location-service/src/main/java/com/rideshare/location_service/service/LocationService.java
+++ location-service/src/main/java/com/rideshare/location_service/service/LocationService.java
@@ -1,5 +1,34 @@
 package com.rideshare.location_service.service;
 
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import com.rideshare.location_service.dto.DriverLocationRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class LocationService {
-    
+
+    private final RedisTemplate<String, String> redisTemplate;
+    // Redis key for all driver locations
+    private static final String DRIVERS_GEO_KEY = "drivers:locations";
+
+    // update driver location in redis
+    // called every 3 second by drivers phone
+    // maps to redis GEOADD command
+
+    public void updateDriverLocation(DriverLocationRequest driverLocationRequest){
+        log.info("updating location for driver : {}", driverLocationRequest.getDriverId());
+
+        Point driverpoint = new Point(
+            driverLocationRequest.getLongitude(), 
+            driverLocationRequest.getLatitude()
+        );
+       redisTemplate.opsForGeo().add(DRIVERS_GEO_KEY, driverpoint,driverLocationRequest.getDriverId()) 
+    }
 }
